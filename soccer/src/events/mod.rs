563c201e3378af4877410bcafea2af5ec2db3e95
Shane Lillie <ignignokterr@gmail.com>
2022-05-03 17:59:58 -0700

    Note

--- soccer/src/events/mod.rs
+++ soccer/src/events/mod.rs
@@ -3,4 +3,8 @@ pub mod messaging;
 
 use bevy::prelude::*;
 
+// this is mainly an event because calling find_support()
+// pollutes systems with so many queries
+// due to needing to potentially update
+// the best supporting spot
 pub struct FindSupportEvent(pub Entity);

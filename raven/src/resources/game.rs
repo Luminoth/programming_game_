72b62086428662aedbc876f58e706d82266dfebd
Shane Lillie <ignignokterr@gmail.com>
2022-06-07 09:32:00 -0700

    Note for later

--- raven/src/resources/game.rs
+++ raven/src/resources/game.rs
@@ -1,5 +1,16 @@
+use crate::components::world::*;
+
 #[derive(Debug)]
 pub struct Map;
+
+impl Map {
+    pub fn get_random_spawnpoint<'a, S>(&self, spawnpoints: S) -> Option<SpawnPoint>
+    where
+        S: Iterator<Item = &'a SpawnPoint>,
+    {
+        todo!();
+    }
+}
 
 #[derive(Debug)]
 pub struct NavGraph;
